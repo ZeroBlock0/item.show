d46747643b62e992c72772aad9785b181b24a5c6
ZeroBlock0 <43921443+ZeroBlock0@users.noreply.github.com>
2025-11-15 03:45:47 +0800

    del

--- anime/tests/playground/sandbox/index.js
+++
@@ -1,5 +0,0 @@
-import { waapi, animate, onScroll, $, set, stagger, spring, random, utils } from '../../../dist/modules/index.js';
-
-async function animation() {
-  await animate('test', { x: 100 });
-}
